e29321abacbf611c792159216111628235621ac2
Chris <chrisaconquet@hotmail.com>
2019-05-16 21:59:21 +0100

    Je kunt nu via de map reizen + volume kan nu worden aangepast via de menu + bug fixes
    menu kan geopend worden door op I te drukken, en om de map te openen druk je op M

--- Assets/Scripts/Dialogue Scripts/DialogueManager.cs
+++ Assets/Scripts/Dialogue Scripts/DialogueManager.cs
@@ -7,24 +7,33 @@ public class DialogueManager : MonoBehaviour
 {
     public Text nameText;
     public Text DialogueText;
-
+    public int currentSentence;
     public Animator animator;
 
-    private Queue<string> sentences;
+    public Queue<string> sentences;
+
+    public DialogueScript dialogueScript;
+
+    public GameObject Characteur1;
+    public GameObject Characteur2;
+
+    private bool StopDialogue;
 
     void Start()
     {
+        //dialogueScript = GetComponent<DialogueScript>();
         sentences = new Queue<string>();
     }
 
     public void StartDialogue (DialogueScript dialogue)
     {
         animator.SetBool("IsOpen", true);
-
         nameText.text = dialogue.name;
         sentences.Clear();
+        Characteur1.SetActive(true);
+        //dialogueScript.Charactuer.SetActive(true);
 
-        foreach(string sentence in dialogue.sentences) {
+        foreach (string sentence in dialogue.sentences) {
             sentences.Enqueue(sentence);
         }
         DisplayNextSentence();
@@ -32,7 +41,7 @@ public class DialogueManager : MonoBehaviour
 
     public void DisplayNextSentence()
     {
-        if(sentences.Count == 0) {
+        if(sentences.Count == currentSentence) {
             EndDialogue();
             return;
         }
@@ -40,20 +49,41 @@ public class DialogueManager : MonoBehaviour
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
         //DialogueText.text = sentence; This displays the text without animation
-        Debug.Log(sentence);
+    }
+
+    public void Update()
+    {
+        string sentence = sentences.Dequeue();
+        StopTyping(sentence);
+        
+    }
+
+    public void StopTyping(string sentence)
+    {
+        if (Input.GetMouseButtonDown(1)) {
+            //StopDialogue = true;
+            Debug.Log("Click");
+            //DialogueText.text = sentence;
+            //StopAllCoroutines();
+            StopDialogue = false;
+        }
     }
 
     IEnumerator TypeSentence(string sentence)
     {
+
         DialogueText.text = "";
         foreach(char letter in sentence.ToCharArray()) {
             DialogueText.text += letter;
             yield return null; //This makes it so that the letters load in per frame
         }
+
+        
     }
 
     public void EndDialogue()
     {
         animator.SetBool("IsOpen", false);
+        //dialogueScript.Charactuer.SetActive(false);
     }
 }
--- Assets/Scripts/Dialogue Scripts/DialogueScript.cs
+++ Assets/Scripts/Dialogue Scripts/DialogueScript.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DialogueScript
 {
     public string name;
+    public GameObject Charactuer;
 
     [TextArea(3, 10)]
     public string[] sentences;
